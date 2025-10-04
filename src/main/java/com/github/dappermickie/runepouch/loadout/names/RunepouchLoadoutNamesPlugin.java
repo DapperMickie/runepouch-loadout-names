c4e902686378d6da8d4950c5ad996ed1e6b0d1a1
Tyler Stewart <git@tylerstewart.ca>
2025-10-03 22:11:49 -0600

    feat: icon selection menu

--- src/main/java/com/github/dappermickie/runepouch/loadout/names/RunepouchLoadoutNamesPlugin.java
+++ src/main/java/com/github/dappermickie/runepouch/loadout/names/RunepouchLoadoutNamesPlugin.java
@@ -4,6 +4,9 @@ import com.google.common.base.Strings;
 import com.google.inject.Provides;
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.List;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
@@ -58,10 +61,95 @@ public class RunepouchLoadoutNamesPlugin extends Plugin
 		ColorUtil.prependColorTag("(Limit %s Characters)", new Color(0, 0, 170));
 	private int lastRunepouchVarbitValue = 0;
 
-	@Override
-	protected void shutDown() throws Exception
+	private int[] getAllSpriteIds()
 	{
-		clientThread.invokeLater(this::resetRunepouchWidget);
+		List<Integer> spriteIds = new ArrayList<>();
+		addStaticIntFields(SpriteID.Magicon2.class, spriteIds);
+		addStaticIntFields(SpriteID.LunarMagicOn.class, spriteIds);
+		addStaticIntFields(SpriteID.MagicNecroOn.class, spriteIds);
+		addStaticIntFields(SpriteID.Prayeron.class, spriteIds);
+		addStaticIntFields(SpriteID.IconPrayerZaros01_30x30.class, spriteIds);
+		addStaticIntFields(SpriteID.IconBoss25x25.class, spriteIds);
+		addStaticIntFields(SpriteID.HiscoresBosses.class, spriteIds);
+		addStaticIntFields(SpriteID.FrexRunes.class, spriteIds);
+		addStaticIntFields(SpriteID.SideIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.Staticons.class, spriteIds);
+		addStaticIntFields(SpriteID.Staticons2.class, spriteIds);
+		addStaticIntFields(SpriteID.AccountIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.OrbIcon.class, spriteIds);
+		addStaticIntFields(SpriteID.ToaInvocationIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.ToaDifficultyIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.HeadiconsPkInterface.class, spriteIds);
+		addStaticIntFields(SpriteID.PvpwIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.PvpaRankicons.class, spriteIds);
+		addStaticIntFields(SpriteID.DeadmanSigilIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.DeadmanSigilSkulls.class, spriteIds);
+		addStaticIntFields(SpriteID.DeadmanSigilCombatIconsSmall.class, spriteIds);
+		addStaticIntFields(SpriteID.DeadmanSigilSkillingIconsSmall.class, spriteIds);
+		addStaticIntFields(SpriteID.DeadmanSigilUtilityIconsSmall.class, spriteIds);
+		addStaticIntFields(SpriteID.IconYama34x34.class, spriteIds);
+		addStaticIntFields(SpriteID.OptionsIconsSmall.class, spriteIds);
+		addStaticIntFields(SpriteID.OptionsIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.Emotes.class, spriteIds);
+		addStaticIntFields(SpriteID.PengEmotes.class, spriteIds);
+		addStaticIntFields(SpriteID.SideiconsInterface.class, spriteIds);
+		addStaticIntFields(SpriteID.SideiconsNew.class, spriteIds);
+		addStaticIntFields(SpriteID.IiImplingIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.Hitmark.class, spriteIds);
+		addStaticIntFields(SpriteID.WintIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.TinyCombatStaticons.class, spriteIds);
+		addStaticIntFields(SpriteID.IconAlchemyChemicals01_27x27.class, spriteIds);
+		addStaticIntFields(SpriteID.Dt2Icons.class, spriteIds);
+		addStaticIntFields(SpriteID.SoulWarsStaticons.class, spriteIds);
+		addStaticIntFields(SpriteID.SoulWarsGameicons.class, spriteIds);
+		addStaticIntFields(SpriteID.IronIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.ModIconsInterface.class, spriteIds);
+		addStaticIntFields(SpriteID.ClanRankIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.Mapfunction.class, spriteIds);
+		addStaticIntFields(SpriteID.Spectator.class, spriteIds);
+		addStaticIntFields(SpriteID.Thumbs.class, spriteIds);
+		addStaticIntFields(SpriteID.GodWarsIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.OsmStatusIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.CaTierSwordsSmall.class, spriteIds);
+		addStaticIntFields(SpriteID.LeagueTaskTiers.class, spriteIds);
+		addStaticIntFields(SpriteID.LeagueTinyRelic.class, spriteIds);
+		addStaticIntFields(SpriteID.LeagueRelics.class, spriteIds);
+		addStaticIntFields(SpriteID.TrailblazerRelics.class, spriteIds);
+		addStaticIntFields(SpriteID.TrailblazerMapShields.class, spriteIds);
+		addStaticIntFields(SpriteID.League3Relics.class, spriteIds);
+		addStaticIntFields(SpriteID.League4Relics.class, spriteIds);
+		addStaticIntFields(SpriteID.League4MapShields01.class, spriteIds);
+		addStaticIntFields(SpriteID.League5Relics.class, spriteIds);
+		addStaticIntFields(SpriteID.League5MapShields01.class, spriteIds);
+		addStaticIntFields(SpriteID.League5CombatMasterySmall.class, spriteIds);
+		addStaticIntFields(SpriteID.League5CombatMasteryTierSmall.class, spriteIds);
+		addStaticIntFields(SpriteID.LeagueTrophyIcons.class, spriteIds);
+		addStaticIntFields(SpriteID.RomanNumerals.class, spriteIds);
+		addStaticIntFields(SpriteID.League3Numerals.class, spriteIds);
+		addStaticIntFields(SpriteID.SotnCipher.class, spriteIds);
+		addStaticIntFields(SpriteID.MorseCode.class, spriteIds);
+
+		return spriteIds.stream()
+			.distinct()
+			.mapToInt(Integer::intValue)
+			.toArray();
+	}
+
+	private void addStaticIntFields(Class<?> clazz, List<Integer> spriteIds)
+	{
+		Field[] fields = clazz.getDeclaredFields();
+		for (Field field : fields) {
+			// Check if field is static and int type
+			if (Modifier.isStatic(field.getModifiers()) && field.getType() == int.class) {
+				try {
+					field.setAccessible(true);
+					int value = field.getInt(null);
+					spriteIds.add(value);
+				} catch (Exception e) {
+					log.warn("Failed to access static int field {} in class {}", field.getName(), clazz.getSimpleName(), e);
+				}
+			}
+		}
 	}
 
 	@Override
@@ -73,6 +161,12 @@ public class RunepouchLoadoutNamesPlugin extends Plugin
 				reloadRunepouchLoadout();
 			}
 		});
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		clientThread.invokeLater(this::resetRunepouchWidget);
 	}
 
 	@Subscribe
@@ -145,7 +239,7 @@ public class RunepouchLoadoutNamesPlugin extends Plugin
 				.setOption("Reset")
 				.setTarget("Icon")
 				.setType(MenuAction.RUNELITE)
-				.onClick((MenuEntry e) -> setLoadoutIcon(loadoutId, SpriteID.AccManIcons._6)));
+				.onClick((MenuEntry e) -> resetLoadoutIcon(loadoutId)));
 		}
 	}
 
@@ -196,24 +290,213 @@ public class RunepouchLoadoutNamesPlugin extends Plugin
 		clientThread.invokeLater(this::reloadRunepouchLoadout);
 	}
 
+	private void resetLoadoutIcon(int id)
+	{
+		var chatContainer = client.getWidget(InterfaceID.Chatbox.UNIVERSE);
+		chatContainer.deleteAllChildren();
+
+		setLoadoutIcon(id, SpriteID.AccManIcons._6);
+	}
+
 	private void changeLoadoutIcon(int id)
 	{
-		int oldLoadoutIcon = getLoadoutIcon(id);
-		chatboxPanelManager.openTextInput(String.format("%s<br>", "Loadout Icon: "))
-			.value(Strings.nullToEmpty(String.valueOf(oldLoadoutIcon)))
-			.onDone((newLoadoutIconStr) ->
-			{
-				if (newLoadoutIconStr == null) return;
+		final int[] availableSpriteIds = getAllSpriteIds();
 
-				newLoadoutIconStr = Text.removeTags(newLoadoutIconStr).trim();
+		final int iconsPerRow = 12;
+		final int iconSize = 28;
+		final int iconSpacing = 12;
 
-				try {
-					var newLoadoutIcon = Integer.parseInt(newLoadoutIconStr);
-					setLoadoutIcon(id, newLoadoutIcon);
-				} catch (NumberFormatException e) {
-					setLoadoutIcon(id, SpriteID.AccManIcons._6);
-				}
-			}).build();
+		var chatContainer = client.getWidget(InterfaceID.Chatbox.UNIVERSE);
+		chatContainer.deleteAllChildren();
+		
+		// Create main layer (overlay)
+		var layer = chatContainer.createChild(-1, WidgetType.LAYER);
+		layer.setWidthMode(WidgetSizeMode.MINUS);
+		layer.setHeightMode(WidgetSizeMode.MINUS);
+		layer.setOriginalHeight(23);
+
+		layer.setXPositionMode(WidgetPositionMode.ABSOLUTE_LEFT);
+		layer.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP);
+		layer.setNoClickThrough(true);
+
+		layer.revalidate();
+
+		// Create background
+		var bg = layer.createChild(-1, WidgetType.GRAPHIC);
+		bg.setSpriteId(1017);
+		bg.setWidthMode(WidgetSizeMode.MINUS);
+		bg.setHeightMode(WidgetSizeMode.MINUS);
+		bg.setXPositionMode(WidgetPositionMode.ABSOLUTE_LEFT);
+		bg.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP);
+		bg.revalidate();
+
+		// Create scrollable container
+		var container = layer.createChild(-1, WidgetType.LAYER);
+		container.setWidthMode(WidgetSizeMode.MINUS);
+		container.setHeightMode(WidgetSizeMode.MINUS);
+		container.setOriginalHeight(12);
+		container.setOriginalWidth(12);
+		container.setXPositionMode(WidgetPositionMode.ABSOLUTE_LEFT);
+		container.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP);
+		container.setOriginalX(6);
+		container.setOriginalY(6);
+		
+		int totalRows = ((availableSpriteIds.length + iconsPerRow + 1) / iconsPerRow) + 1; 
+		int scrollHeight = Math.max(0, (totalRows * (iconSize + iconSpacing)) - 200);
+
+		container.setScrollHeight(scrollHeight);
+		container.setHasListener(true);
+		container.revalidate();
+
+		// Create scrollbar
+		var scrollbarContainer = layer.createChild(-1, WidgetType.LAYER);
+		scrollbarContainer.setWidthMode(WidgetSizeMode.ABSOLUTE);
+		scrollbarContainer.setHeightMode(WidgetSizeMode.MINUS);
+		scrollbarContainer.setOriginalWidth(16);
+		scrollbarContainer.setOriginalHeight(14);
+		scrollbarContainer.setXPositionMode(WidgetPositionMode.ABSOLUTE_RIGHT);
+		scrollbarContainer.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP);
+		scrollbarContainer.setOriginalY(8);
+		scrollbarContainer.setOriginalX(6);
+		scrollbarContainer.revalidate();
+
+		var scrollbarBG = scrollbarContainer.createChild(-1, WidgetType.GRAPHIC);
+		scrollbarBG.setWidthMode(WidgetSizeMode.ABSOLUTE);
+		scrollbarBG.setHeightMode(WidgetSizeMode.MINUS);
+		scrollbarBG.setOriginalWidth(16);
+		scrollbarBG.setOriginalHeight(32);
+		scrollbarBG.setSpriteId(792);
+		scrollbarBG.setXPositionMode(WidgetPositionMode.ABSOLUTE_RIGHT);
+		scrollbarBG.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP);
+		scrollbarBG.setOriginalY(16);
+		scrollbarBG.setOriginalX(0);
+		scrollbarBG.revalidate();
+
+		var scrollUpButton = scrollbarContainer.createChild(-1, WidgetType.GRAPHIC);
+		scrollUpButton.setWidthMode(WidgetSizeMode.ABSOLUTE);
+		scrollUpButton.setHeightMode(WidgetSizeMode.ABSOLUTE);
+		scrollUpButton.setOriginalWidth(16);
+		scrollUpButton.setOriginalHeight(16);
+		scrollUpButton.setSpriteId(773);
+		scrollUpButton.setXPositionMode(WidgetPositionMode.ABSOLUTE_RIGHT);
+		scrollUpButton.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP);
+		scrollUpButton.setOriginalY(0);
+		scrollUpButton.setOriginalX(0);
+		scrollUpButton.setHasListener(true);
+		scrollUpButton.setNoClickThrough(true);
+		scrollUpButton.setAction(0, "Up");
+		scrollUpButton.setOnOpListener((JavaScriptCallback) (ScriptEvent event) -> {
+			int currentScrollY = container.getScrollY();
+			int newScrollY = Math.max(0, currentScrollY - 30);
+			container.setScrollY(newScrollY);
+		});
+		scrollUpButton.revalidate();
+
+		var scrollDownButton = scrollbarContainer.createChild(-1, WidgetType.GRAPHIC);
+		scrollDownButton.setWidthMode(WidgetSizeMode.ABSOLUTE);
+		scrollDownButton.setHeightMode(WidgetSizeMode.ABSOLUTE);
+		scrollDownButton.setOriginalWidth(16);
+		scrollDownButton.setOriginalHeight(16);
+		scrollDownButton.setSpriteId(788);
+		scrollDownButton.setXPositionMode(WidgetPositionMode.ABSOLUTE_RIGHT);
+		scrollDownButton.setYPositionMode(WidgetPositionMode.ABSOLUTE_BOTTOM);
+		scrollDownButton.setOriginalY(0);
+		scrollDownButton.setOriginalX(0);
+		scrollDownButton.setHasListener(true);
+		scrollDownButton.setNoClickThrough(true);
+		scrollDownButton.setAction(0, "Down");
+		scrollDownButton.setOnOpListener((JavaScriptCallback) (ScriptEvent event) -> {
+			int maxScrollHeight = container.getScrollHeight();
+			int currentScrollY = container.getScrollY();
+			int newScrollY = Math.min(currentScrollY + 30, maxScrollHeight);
+			container.setScrollY(newScrollY);
+		});
+		scrollDownButton.revalidate();
+
+		var scrollBarHandle = scrollbarContainer.createChild(-1, WidgetType.GRAPHIC);
+		scrollBarHandle.setSpriteId(790);
+		scrollBarHandle.setWidthMode(WidgetSizeMode.ABSOLUTE);
+		scrollBarHandle.setHeightMode(WidgetSizeMode.ABSOLUTE);
+		scrollBarHandle.setOriginalWidth(16);
+		scrollBarHandle.setOriginalHeight(16);
+		scrollBarHandle.setXPositionMode(WidgetPositionMode.ABSOLUTE_RIGHT);
+		scrollBarHandle.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP);
+		scrollBarHandle.setOriginalY(16);
+		scrollBarHandle.setOriginalX(0);
+		scrollBarHandle.setHasListener(true);
+		scrollBarHandle.setNoClickThrough(true);
+		scrollBarHandle.setDragParent(scrollbarBG);
+		scrollBarHandle.setOnDragListener((JavaScriptCallback) (ScriptEvent event) -> {
+			var scrollPercentage = (double)event.getMouseY() / 80.0;
+			var maxScrollHeight = container.getScrollHeight();
+			var newScrollY = (int) (scrollPercentage * maxScrollHeight);
+			container.setScrollY(Math.max(0, Math.min(newScrollY, maxScrollHeight)));
+		});
+		scrollBarHandle.setOnDragCompleteListener((JavaScriptCallback) (ScriptEvent event) -> {
+			var scrollPercentage = (double)event.getMouseY() / 80.0;
+			var scrollBarHandleY = (int) (scrollPercentage * (scrollbarBG.getHeight() + 32));
+			scrollBarHandle.setOriginalY(Math.max(16, Math.min(scrollBarHandleY, scrollbarBG.getHeight())));
+			scrollBarHandle.revalidate();
+		});
+		scrollBarHandle.revalidate();
+
+		container.setOnScrollWheelListener((JavaScriptCallback) (ScriptEvent event) -> {
+			int currentScrollY = container.getScrollY();
+			int scrollAmount = 30 * event.getMouseY();
+			int newScrollY = Math.max(0, Math.min(currentScrollY + scrollAmount, scrollHeight));
+			container.setScrollY(newScrollY);
+
+			var scrollBarPercentage = ((double)newScrollY / (double)scrollHeight);
+			var scrollBarHandleY = (int) (scrollBarPercentage * scrollbarBG.getHeight()) + 8;
+			scrollBarHandle.setOriginalY(Math.max(16, Math.min(scrollBarHandleY, scrollbarBG.getHeight())));
+			scrollBarHandle.revalidate();
+		});
+		container.revalidate();
+
+		for (int i = 0; i < availableSpriteIds.length; i++) {
+			int spriteId = availableSpriteIds[i];
+			int row = i / iconsPerRow;
+			int col = i % iconsPerRow;
+			
+			// Create icon button
+			var iconButton = container.createChild(-1, WidgetType.GRAPHIC);
+			iconButton.setSpriteId(spriteId);
+			iconButton.setWidthMode(WidgetSizeMode.ABSOLUTE);
+			iconButton.setHeightMode(WidgetSizeMode.ABSOLUTE);
+			iconButton.setOriginalWidth(iconSize);
+			iconButton.setOriginalHeight(iconSize);
+			iconButton.setXPositionMode(WidgetPositionMode.ABSOLUTE_LEFT);
+			iconButton.setYPositionMode(WidgetPositionMode.ABSOLUTE_TOP);
+			iconButton.setOriginalX(col * (iconSize + iconSpacing) + iconSpacing);
+			iconButton.setOriginalY(row * (iconSize + iconSpacing) + iconSpacing);
+			iconButton.setHasListener(true);
+			iconButton.setNoClickThrough(false);
+			iconButton.setNoScrollThrough(false);
+			iconButton.setAction(0, "Set Icon (" + spriteId + ")");
+			iconButton.setTargetVerb(String.valueOf(spriteId));
+			iconButton.setOnOpListener((JavaScriptCallback) (ScriptEvent event) -> {
+				setLoadoutIcon(id, spriteId);
+				chatContainer.deleteAllChildren();
+			});
+			
+			// Add hover effect
+			iconButton.setOnMouseRepeatListener((JavaScriptCallback) (ScriptEvent event) -> {
+				iconButton.setOpacity(80);
+				iconButton.setOriginalWidth(iconSize + 2);
+				iconButton.setOriginalHeight(iconSize + 2);
+				iconButton.setOriginalX(col * (iconSize + iconSpacing) + iconSpacing - 1);
+				iconButton.setOriginalY(row * (iconSize + iconSpacing) + iconSpacing - 1);
+				iconButton.revalidate();
+			});
+			iconButton.setOnMouseLeaveListener((JavaScriptCallback) (ScriptEvent event) -> {
+				iconButton.setOpacity(0);
+				iconButton.setOriginalWidth(iconSize);
+				iconButton.setOriginalHeight(iconSize);
+				iconButton.revalidate();
+			});
+			
+			iconButton.revalidate();
+		}
 	}
 
 	@Subscribe
